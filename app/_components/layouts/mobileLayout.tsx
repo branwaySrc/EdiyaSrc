e882887885950feb3c6bd78f7db2f39e90fe0043
milliceo <millimceo@gmail.com>
2025-07-26 13:24:39 +0900

    error-screen mobile-size reorganized into max-lg

--- app/_components/layouts/mobileLayout.tsx
+++ app/_components/layouts/mobileLayout.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from "react";
 export default function MobileLayout(props: PropsWithChildren): React.ReactNode {
 	return (
 		<div className="w-full min-h-screen flex justify-center bg-blue-200">
-			<div className="w-full max-w-sm mx-auto bg-white relative tracking-tight">{props.children}</div>
+			<div className="w-full min-w-sm max-w-xl mx-auto bg-white relative tracking-tight">{props.children}</div>
 		</div>
 	);
 }
